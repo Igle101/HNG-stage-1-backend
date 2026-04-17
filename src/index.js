5cc02addd78208f4e5f25fd6d9877e4675ec8e88
igle_code <emilymaze12@gmail.com>
2026-04-17 22:39:13 +0100

    fix:dotenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
-const express = require('express');
 require('dotenv').config();
+console.log('MONGO URI:', process.env.MONGODB_URI ? 'loaded' : 'NOT FOUND');
+const express = require('express');
 
 const app = express();
 const connectDB =require('./config/db')
